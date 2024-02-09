17eee66f43530b5374e6ed932aa0da72d7a67c97
GadiegoN <ngadiego@gmail.com>
2024-02-09 20:41:15 -0300

    feat: Add the Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { Toaster } from 'sonner'
 import './index.css'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <DndProvider backend={HTML5Backend}>
       <App />
       <Toaster richColors />
+      <Analytics />
     </DndProvider>
   </React.StrictMode>,
 )
